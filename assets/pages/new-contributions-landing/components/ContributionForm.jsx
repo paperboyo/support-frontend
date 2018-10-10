44c978c8981202c96f9dfa7059bf00506a497bcd
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2018-10-10 15:13:00 +0100

    refactored form container

--- assets/pages/new-contributions-landing/components/ContributionForm.jsx
+++ assets/pages/new-contributions-landing/components/ContributionForm.jsx
@@ -228,7 +228,9 @@ function ContributionForm(props: PropTypes) {
       />
       <ContributionFormFields />
       <NewContributionPayment onPaymentAuthorisation={onPaymentAuthorisation} />
-      <NewContributionSubmit />
+      <NewContributionSubmit
+        whenUnableToOpen={props.setCheckoutFormHasBeenSubmitted}
+      />
       {props.isWaiting ? <ProgressMessage message={['Processing transaction', 'Please wait']} /> : null}
     </form>
   );
